5e8bed3299d8c1aee75962511a0370938dddb7cf
wahyukurniaa <wahyukurnia321123@gmail.com>
2024-02-01 22:37:24 +0700

    get-data-image-&-author

---
+++ lib/app/modules/articles/controller/article_controller.dart
@@ -0,0 +1,47 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+// import 'package:hitapi/app/modules/articles/models/ArticleModels.dart';
+import 'package:http/http.dart' as http;
+
+class ArticleController extends GetxController {
+  RxList<dynamic> results = RxList<dynamic>([]);
+
+  void getData() async {
+    var uri = Uri.parse(
+        'https://newsapi.org/v2/everything?q=tesla&from=2024-01-01&sortBy=publishedAt&apiKey=7981d6f5e59849e5baa49b5f92003006');
+    var response = await http.get(uri);
+
+    if (response.statusCode == 200) {
+      var data = jsonDecode(response.body);
+      if (data['articles'] != null) {
+        results.value = data['articles'] as List<dynamic>;
+        // debugPrint(results.value.toString());
+      } else {
+        debugPrint('Data "articles" tidak ditemukan dalam respons API.');
+      }
+    } else {
+      debugPrint('Request failed with status: ${response.statusCode}');
+    }
+  }
+}
+
+
+// class UserController extends GetxController {
+//   RxList<dynamic> results = RxList<dynamic>([]);
+
+//   void getUser() async {
+//     var uri = Uri.parse('https://jsonplaceholder.typicode.com/users');
+//     var response = await http.get(uri);
+
+//     if (response.statusCode == 200) {
+//       var data = response.body;
+//       debugPrint('data: ${response.body}');
+
+//       results.value = jsonDecode(data) as List<dynamic>;
+//     } else {
+//       debugPrint('Request failed with status: ${response.statusCode}');
+//     }
+//   }
+// }
--- lib/app/modules/articles/page/articel_screen.dart
+++ lib/app/modules/articles/page/articel_screen.dart
@@ -1,13 +1,45 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:hitapi/app/modules/articles/controller/article_controller.dart';
 
 class ArticleSCreen extends StatelessWidget {
   const ArticleSCreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    final controller = Get.put(ArticleController());
+
+    Future.delayed(Duration.zero, () async {
+      controller.getData();
+    });
+    return Scaffold(
       body: Center(
-        child: Text('Article Screen'),
+        child: Obx(
+          () => ListView.builder(
+            itemCount: controller.results.length,
+            itemBuilder: (context, index) {
+              var item = controller.results[index];
+              return Padding(
+                padding: const EdgeInsets.only(bottom: 16),
+                child: ListTile(
+                  title: Text(controller.results[index]['author'] ?? ''),
+                  leading: Container(
+                    width: 100,
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        fit: BoxFit.cover,
+                        image: item['urlToImage'] != null
+                            ? NetworkImage(item['urlToImage'])
+                            : const NetworkImage(
+                                'https://d1vbn70lmn1nqe.cloudfront.net/prod/wp-content/uploads/2023/03/13032551/Kenali-X-Jenis-dan-Manfaat-Pisang-untuk-Kesehatan-Tubuh.jpg.webp'),
+                      ),
+                    ),
+                  ),
+                ),
+              );
+            },
+          ),
+        ),
       ),
     );
   }
